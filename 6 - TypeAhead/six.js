6700daa367aedf91021169afb5bfcb0c71add626
Garry Desktop <gtoborn@gmail.com>
2018-01-17 17:00:49 -0700

    Made changes to six.js file

--- 6 - TypeAhead/six.js
+++ 6 - TypeAhead/six.js
@@ -1,5 +1,3 @@
-
-
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
 const cities = [];
@@ -10,13 +8,24 @@ fetch(endpoint)
 
 function findMatches(wordToMatch, cities) {
     return cities.filter(place => {
+        //figure out if city or state matches what was searched
         const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.state.match(regex);
+        return place.city.match(regex) || place.state.match(regex)
     });
 }
 
 function displayMatches() {
-    console.log(this.value);
+    const matchArray = findMatches(this.value, cities);
+    // console.log(matchArray);
+    const html = matchArray.map(place => {
+        return `
+            <li>
+                <span class="name">${place.city}, ${place.state}</span>
+                <span class="population">${place.population}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
 }
 
 const searchInput = document.querySelector('.search');
